b738b5f50dfbfcf30f0b2756acfa5bb0292d0577
duc <nicolas.duc@openwide.fr>
2015-07-17 10:57:04 +0200

    Update classname

--- DependencyInjection/OpenWidePublishServiceExtension.php
+++ DependencyInjection/OpenWidePublishServiceExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class OpenWideServiceExtension extends Extension
+class OpenWidePublishServiceExtension extends Extension
 {
     /**
      * {@inheritDoc}
